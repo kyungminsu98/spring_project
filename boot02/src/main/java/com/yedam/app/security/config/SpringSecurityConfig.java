6aa9d2db07174f0c8d6465fc980c1eb6616e4339
{kyungminsu98} <admin@YD05-21>
2024-06-05 17:48:13 +0900

    240605

---
+++ boot02/src/main/java/com/yedam/app/security/config/SpringSecurityConfig.java
@@ -0,0 +1,52 @@
+package com.yedam.app.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfig {
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	// 메모리상 인증정보 등록 => 테스트 전용 방식
+	@Bean
+	InMemoryUserDetailsManager InMemoryUserDetailsService() {
+		UserDetails user = User.builder()
+				.username("user1")
+				.password(passwordEncoder().encode("1234"))
+				.roles("USER") // ROLE_USER
+				.authorities("ROLE_USER")
+				.build();
+		UserDetails admin = User.builder()
+				.username("admin1")
+				.password(passwordEncoder().encode("1234"))
+//				.roles("USER") // ROLE_USER
+				.authorities("ROLE_ADMIN", "ROLE_USER")
+				.build();
+		return new InMemoryUserDetailsManager(user, admin);
+	}
+	// 인증 및 인가 설정
+	@Bean
+	SecurityFilterChain filterChin(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests()
+			.antMatchers("/", "/all").permitAll()
+			.antMatchers("/user/**").hasAnyRole("USER", "ADMIN")
+			.antMatchers("/admin/**").hasAuthority("ROLE_ADMIN")
+			.anyRequest().authenticated()
+			.and().formLogin()
+			.and().logout();
+		return http.build();
+			
+	}
+}
--- boot02/src/test/java/com/yedam/app/TestExample.java
+++ boot02/src/test/java/com/yedam/app/TestExample.java
@@ -3,6 +3,7 @@ package com.yedam.app;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.yedam.app.aop.service.AaaService;
 
@@ -11,8 +12,21 @@ public class TestExample {
 	@Autowired
 	AaaService aaaService;
 	
-	@Test
+	//@Test
 	public void aaaInsert() {
 		aaaService.insert();
+	}
+	
+	@Autowired
+	PasswordEncoder passwordEncoder;
+	
+	@Test
+	public void testEncoder() {
+		String password = "1234"; // 사용자가 입력
+		// DB에 저장된 암호화된 비밀번호
+		String enPwd = passwordEncoder.encode(password);
+		System.out.println("enPwd : " + enPwd);
+		boolean matchResult = passwordEncoder.matches(password, enPwd);
+		System.out.println("match : " + matchResult);
 	}
 }
